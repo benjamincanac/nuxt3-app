a2c1e60309c4a20b1b914ffcf6a695f643434409
Benjamin Canac <canacb1@gmail.com>
2021-11-24 14:33:56 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mx-auto max-w-80em">
+    Welcome
+    <UButton class="ml-3" variant="primary" icon="heroicons-outline:check-circle">
+      toto
+    </UButton>
+
+    <UInput name="search" />
+
+    <div>
+      <div class="w-2 h-2 bg-primary-600" />
+    </div>
+  </div>
+</template>
